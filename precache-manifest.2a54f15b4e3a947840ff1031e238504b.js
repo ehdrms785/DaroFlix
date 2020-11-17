5a05d0b2326ad53c5f5c37ff530d93b36eb2022a
Daro <ehdrms785@naver.com>
2020-11-17 10:28:12 +0900

    Updates

---
+++ precache-manifest.2a54f15b4e3a947840ff1031e238504b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae762fb58f3ebfe6c7d9b8d487ee40fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad9652030c7906d3acde",
+    "url": "/static/js/2.26bbd353.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.26bbd353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaec070298c4c8b7509a",
+    "url": "/static/js/main.cb60c7c3.chunk.js"
+  },
+  {
+    "revision": "ca8db48cdf2e3bfa8d7a",
+    "url": "/static/js/runtime-main.6ffe6b6e.js"
+  },
+  {
+    "revision": "ce572da35e3c82de823e8a81cd14ab30",
+    "url": "/static/media/imdb.ce572da3.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DaroFlix/precache-manifest.72cba35ab54963c0bbd7438b679fab91.js"
+  "/precache-manifest.2a54f15b4e3a947840ff1031e238504b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DaroFlix/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
